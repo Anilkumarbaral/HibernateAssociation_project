b5a0d66e4c5ffc319bc76f6d8a69f90eaf272bd2
anilkumarbaral <anilbaral06@gmail.ocm>
2024-02-11 10:34:32 +0530

    m2 method created

--- abc.java
+++ abc.java
@@ -2,6 +2,10 @@ class A
 {
   public static void main(String []args){
 	// i have got task to work bug001
-    
-  }
+
+    public void m2(){
+		
+     System.out.println("This is m2 called...");
+    }
+  } 
 }
