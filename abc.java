5bd1f20e4cda915e20a512810a82ef35e58ae718
anilkumarbaral <anilbaral06@gmail.ocm>
2024-02-11 10:56:24 +0530

    added

--- abc.java
+++ abc.java
@@ -3,9 +3,17 @@ class A
   public static void main(String []args){
 	// i have got task to work bug001
 
-    public void m2(){
+
+    
+  } 
+public void m2(){
 		
      System.out.println("This is m2 called...");
     }
-  } 
+   public void m1(){
+	
+   }
+    
+  }
+>>>>>>> Stashed changes
 }
